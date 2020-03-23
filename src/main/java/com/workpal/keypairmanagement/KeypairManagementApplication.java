6a5057a342515807ec09fcdb3a48cf500f8a7523
narendra.kumar <kumar.adobe@gmail.com>
2020-03-23 22:38:15 +0530

    generated project using spring initializr

---
+++ src/main/java/com/workpal/keypairmanagement/KeypairManagementApplication.java
@@ -0,0 +1,13 @@
+package com.workpal.keypairmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeypairManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeypairManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workpal/keypairmanagement/KeypairManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workpal.keypairmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeypairManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
