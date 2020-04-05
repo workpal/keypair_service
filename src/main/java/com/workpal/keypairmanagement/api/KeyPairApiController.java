26834b9fb3d9815b3c7aeb0c189d475e63dffbee
Ramadhas <Ramadhas>
2020-04-05 22:37:57 +0530

    Keypair API document added

--- src/main/java/com/workpal/keypairmanagement/api/KeyPairApiController.java
+++ src/main/java/com/workpal/keypairmanagement/api/KeyPairApiController.java
@@ -16,8 +16,13 @@ import com.workpal.keypairmanagement.request.GenerateKeyPairRequest;
 import com.workpal.keypairmanagement.request.KeyPairCreateRequest;
 import com.workpal.keypairmanagement.service.KeyPairService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController()
 @CrossOrigin
+@Api(value = "Keypair", description = "Set of endpoints for Creating, Retrieving of Keypairs.")
 public class KeyPairApiController implements KeyPairApi	 {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(KeyPairApiController.class);
@@ -26,27 +31,31 @@ public class KeyPairApiController implements KeyPairApi	 {
 	private KeyPairService keyPairService;
 
 	@Override
-	public ResponseEntity<?> createKeyPair(@Valid @RequestBody KeyPairCreateRequest keyPairCreateRequest) {
+	@ApiOperation("Creates a new keypair.")
+	public ResponseEntity<?> createKeyPair(@ApiParam("Request of create a new keypair ") @Valid @RequestBody KeyPairCreateRequest keyPairCreateRequest) {
 		LOGGER.info("Create key pair {} ",keyPairCreateRequest);
 		keyPairService.createKeyPair(keyPairCreateRequest);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@Override
-	public ResponseEntity<?> generateKeyPair(@Valid @RequestBody GenerateKeyPairRequest generateKeyPairRequest) {
+	@ApiOperation("Generate a new keypair.")
+	public ResponseEntity<?> generateKeyPair(@ApiParam("Request of generate a new keypair ") @Valid @RequestBody GenerateKeyPairRequest generateKeyPairRequest) {
 		LOGGER.info("Generate key pair {} ",generateKeyPairRequest);
 		keyPairService.generateKeyPair(generateKeyPairRequest);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@Override
+	@ApiOperation("Returns list of all keypairs in the system.")
 	public ResponseEntity<?> getAllKeyPair() {
 		var keyPairs = keyPairService.getAllKeyPairs(); 		
 		return new ResponseEntity<>(keyPairs, HttpStatus.OK);
 	}
 
 	@Override
-	public ResponseEntity<?> getKeyPairById(@PathVariable(value = "keyPairId") String keyPairId) {
+	@ApiOperation("Returns a specific keypair by their identifier. 404 if does not exist.")
+	public ResponseEntity<?> getKeyPairById(@ApiParam("Id of the keypair to be obtained. Cannot be empty.") @PathVariable(value = "keyPairId") String keyPairId) {
 		LOGGER.info("Key pair Id {} ",keyPairId);
 		var keyPair = keyPairService.getKeyPairById(keyPairId);
 		return new ResponseEntity<>(keyPair, HttpStatus.OK);
